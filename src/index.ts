eb390a07b835e0e6246ac7fa0a9ecd2ad9bde055
VolodymyrKornetskyi <volodymyr.kornetskyi@gmail.com>
2018-07-02 19:45:07 +0300

    default settings

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+/*
+ * Copyright (c) Created by Volodymyr Kornetskyi 2/7/2018.
+ */
+// const config = require('config')
+const Koa = require('koa');
+const app = new Koa();
+
+// app.listen(process.env.PORT || config.server.port);
